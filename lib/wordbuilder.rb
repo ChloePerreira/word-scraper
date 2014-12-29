01fb5ea38630877e42017c6d0ae0b4db7158f004
Crystal Perreira <cvperreira@gmail.com>
2014-12-28 20:04:17 -0800

    some methods

--- lib/wordbuilder.rb
+++ lib/wordbuilder.rb
@@ -7,4 +7,21 @@ class WordBuilder
     @words = WordBuilder.new(word_array)
   end
  
+  def create
+  end
+
+  def name
+  end
+
+  def syllable_count
+  end
+
+  def syllable_emphasis
+  end 
+
+  def rank
+  end
+
+  def pronunciation
+  end
 end
